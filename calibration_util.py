81411228d191ceeda77c41a7f557983f98aef297
Gregory Sanchez <sanchez.gregory886@gmail.com>
2020-01-29 14:41:07 -0800

    Add calibration utility

---
+++ calibration_util.py
@@ -0,0 +1,13 @@
+import json
+import numpy as np
+from typing import Tuple
+
+
+def get_mtx_dist() -> Tuple[np.ndarray, np.ndarray]:
+    with open("./data/mtx.json", "r") as fp:
+        mtx = np.asarray(json.load(fp))
+
+    with open("./data/dist.json", "r") as fp:
+        dist = np.asarray(json.load(fp))
+
+    return mtx, dist
